abe28b0bf80f2fce2777f673aa4e1e863ec9ae64
Yubo <you-2001-3-6@ezweb.ne.jp>
2020-12-10 14:18:43 +0900

    complete RFRGS

--- randomforestcv.py
+++ randomforestcv.py
@@ -4,66 +4,78 @@ except ImportError:
     from typing import Any, AnyStr, Dict, Generic, Iterable, Optional, Union, List, TypeVar
 from dataclasses import dataclass, field, InitVar
 from itertools import product
+import re
+from tqdm import tqdm
 
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 
 T = TypeVar('T')
 
 @dataclass
-class RandomForestCV(Generic[T]):
-    n_estimators: InitVar[List[Union[AnyStr, int]]] = ['warn']
-    criterion: InitVar[List[AnyStr]] = ["mse"]
-    max_depth: InitVar[List[Optional[int]]] = [None]
-    min_samples_split: InitVar[List[int]] = [2]
-    min_samples_leaf: InitVar[List[int]] = [1]
-    min_weight_fraction_leaf: InitVar[List[int]] = [0.]
-    max_features: InitVar[List[Union[AnyStr, int]]] = ["auto"]
-    max_leaf_nodes: InitVar[List[Optional[int]]] = [None]
-    min_impurity_decrease: InitVar[List[float]] = [0.]
-    min_impurity_split: InitVar[List[Optional[int]]] = [None]
-    bootstrap: InitVar[List[bool]] = [True]
-    oob_score: InitVar[List[bool]] = [False]
-    n_jobs: InitVar[List[Optional[int]]] = [None]
-    random_state: InitVar[List[Optional[int]]] = [None]
-    verbose: InitVar[List[int]] = [0]
-    warm_start: InitVar[List[bool]] = [False]
-    bestmodel: Any = None
-    bestscore: Optional[float] = None
+class RandomForestRegressorGS(Generic[T]):
+    n_estimators: List[Union[AnyStr, int]] = field(default_factory=lambda: ['warn'])
+    criterion: List[AnyStr] = field(default_factory=lambda: ["mse"])
+    max_depth: List[Optional[int]] = field(default_factory=lambda: [None])
+    min_samples_split: List[int] = field(default_factory=lambda: [2])
+    min_samples_leaf: List[int] = field(default_factory=lambda: [1])
+    min_weight_fraction_leaf: List[int] = field(default_factory=lambda: [0.])
+    max_features: List[Union[AnyStr, int]] = field(default_factory=lambda: ["auto"])
+    max_leaf_nodes: List[Optional[int]] = field(default_factory=lambda: [None])
+    min_impurity_decrease: List[float] = field(default_factory=lambda: [0.])
+    min_impurity_split: List[Optional[int]] = field(default_factory=lambda: [None])
+    bootstrap: List[bool] = field(default_factory=lambda: [True])
+    oob_score: List[bool] = field(default_factory=lambda: [False])
+    n_jobs: List[Optional[int]] = field(default_factory=lambda: [None])
+    random_state: List[Optional[int]] = field(default_factory=lambda: [None])
+    verbose: List[int] = field(default_factory=lambda: [0])
+    warm_start: List[bool] = field(default_factory=lambda: [False])
+    _bestmodel: Any = None
+    _bestscore: Optional[float] = None
+    _best_params: Optional[List] = None
 
 
-    def fit(self, X, y, X_eval, y_eval) -> T:
-        instant_kwargs: Dict[Any]= vars(self)
-        kwargs: Dict[Iterable] = instant_kwargs.copy()
-        basemodel: Optional[RandomForestRegressor] = None
-        for k, v in instant_kwargs.items():
-            if not isinstance(v, Iterable):
+    def fit(self, X, y) -> T:
+        kwargs_instance: Dict[Iterable] = vars(self)
+        kwargs: Dict[Iterable] = kwargs_instance.copy()
+        for k, v in kwargs_instance.items():
+            if re.match('\_', k):
                 kwargs.pop(k)
-
-        COMB: List[Dict[Any]] = product(*kwargs)
-
-        for values in COMB:
-            params: Dict[Any] = {k: v for k, v in zip(kwargs.keys(), values)}
-
-            basemodel = RandomForestRegressor(**params).fit(X, y)
-            
-            score = basemodel.score(X_eval, y_eval)
-
-            if self.bestscore is None:
-                self.bestscore = score
-                self.bestmodel = basemodel
             else:
-                if score > self.bestscore:
-                    self.bestscore = score
-                    self.bestmodel = basemodel
+                if not isinstance(v, Iterable):
+                    kwargs[k] = [v]
+
+        p = product(*(kwargs.values()))
+        for comb in tqdm(p):
+            params = {k: v for k, v in zip(kwargs.keys(), comb)}
+            rfr: RandomForestRegressor = RandomForestRegressor(**params)
+            rfr.fit(X, y)
+
+            score = rfr.score(X, y)
+            if self._bestscore is None:
+                self._bestscore = score
+                self._bestmodel = rfr
+            else:
+                if score > self._bestscore:
+                    self._bestscore = score
+                    self._bestmodel = rfr
 
         return self
 
     def predict(self, X) -> Iterable:
-        pred = self.bestmodel.predict(X)
+        pred = self._bestmodel.predict(X)
 
         return pred
 
     def score(self, X, y) -> float:
-        score = self.bestmodel.score(X, y)
+        score = self._bestmodel.score(X, y)
 
         return score
+
+    @property
+    def bestmodel(self):
+        return self._bestmodel
+    
+    @property
+    def bestscore(self):
+        return self._bestscore
